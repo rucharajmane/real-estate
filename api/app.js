de204b80e05edc075729a70122d34fc79b6b9093
rucharajmane <rucharajmane18@gmail.com>
2024-07-28 12:42:26 +0530

    Setting up the project

---
+++ api/app.js
@@ -0,0 +1,10 @@
+import express from "express";
+import postRoute from "./routes/post_route.js";
+
+const app = express();
+
+app.use("/api/posts", postRoute);
+
+app.listen(8800, () => {
+  console.log("Server is running");
+});
