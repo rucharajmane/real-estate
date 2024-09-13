ac98e8718a094215d3561a7412278f50cf240499
rucharajmane <rucharajmane18@gmail.com>
2024-09-13 14:17:03 +0530

    Registration Done

---
+++ client/src/routes/register/register.jsx
@@ -0,0 +1,28 @@
+import "./register.scss";
+import { Link } from "react-router-dom";
+
+function Register() {
+  const handleSubmit = (e) =>{
+
+  };
+  
+  return (
+    <div className="register">
+      <div className="formContainer">
+        <form onSubmit={handleSubmit}>
+          <h1>Create an Account</h1>
+          <input name="username" type="text" placeholder="Username" />
+          <input name="email" type="text" placeholder="Email" />
+          <input name="password" type="password" placeholder="Password" />
+          <button >Register</button>
+          <Link to="/login">Do you have an account?</Link>
+        </form>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default Register;
