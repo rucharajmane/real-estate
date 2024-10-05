626b35e8fa4d0e1366add503532ca618b34b9f38
rucharajmane <rucharajmane18@gmail.com>
2024-10-05 19:19:30 +0530

    Update profile page is done

--- api/app.js
+++ api/app.js
@@ -1,20 +1,24 @@
 import express from "express";
 import cors from "cors";
-import cookieParser from 'cookie-parser';
+import cookieParser from "cookie-parser";
 import authRoute from "./routes/auth.route.js";
 import postRoute from "./routes/post.route.js";
 import testRoute from "./routes/test.route.js";
+import userRoute from "./routes/user.route.js";
+
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
-app.use(cors({origin : process.env.CLIENT_URL , credentials : true}))
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/test", testRoute);
-
+app.use("/api/user", userRoute);
 
 app.listen(8800, () => {
   console.log("Server is running");
--- api/controllers/test.controlller.js
+++ api/controllers/test.controlller.js
@@ -1,23 +1,21 @@
 import jwt from "jsonwebtoken";
 
-
-export const ShouldBeLoggedIn = async(req,res) =>{
-
-    console.log(req.userId); 
-    res.status(200).json({message : "You are Authenticated"});
+export const ShouldBeLoggedIn = async (req, res) => {
+  console.log(req.userId);
+  res.status(200).json({ message: "You are Authenticated" });
 };
-  
-export const ShouldBeAdmin = async(req,res) => {
-    const token = req.cookies.token
 
-    if(!token) return res.status(401).json({ message : "Not Authenticated!"});
+export const ShouldBeAdmin = async (req, res) => {
+  const token = req.cookies.token;
 
-    jwt.verify(token, process.env.JWT_SECRET_KEY , async(err , payload) => {
-        if(err) return res.status(403).json({ message : "Token  iS not valid"});
-        if(!payload){
-            return res.status(403).json({ message : "not autherised !"});
-        }
-    });
+  if (!token) return res.status(401).json({ message: "Not Authenticated!" });
 
-    res.status(200).json({message : "You are Authenticated"});
-};  
+  jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
+    if (err) return res.status(403).json({ message: "Token  iS not valid" });
+    if (!payload) {
+      return res.status(403).json({ message: "Not authorized!" });
+    }
+  });
+
+  res.status(200).json({ message: "You are Authenticated" });
+};
